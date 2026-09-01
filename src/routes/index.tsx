a96281c451cb8fa9f5c26436d4d5284fa013eed2
Obinarvaez <mgnarvaez@gmail.com>
2026-09-01 16:27:35 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { AdminPanel } from '../components/Dashboard/AdminPanel'
+import { App } from '../App'
 
 export const Route = createFileRoute('/')({
-  component: () => <AdminPanel />,
+  component: () => <App />,
 })
